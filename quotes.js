fd13065de1633aaeb0a2f63bd6f6fb1d85bc4cd2
sombit-k <sombitkarmakar018@gmail.com>
2024-07-05 11:52:04 +0530

    final form

--- quotes.js
+++ quotes.js
@@ -299,5 +299,305 @@ let qList=
       "a": "Seneca",
       "c": "54",
       "h": "\u003Cblockquote\u003E&ldquo;We suffer more often in imagination than in reality.  &rdquo; &mdash; \u003Cfooter\u003ESeneca\u003C/footer\u003E\u003C/blockquote\u003E"
-    }
+    },
+    {
+        "q": "Sad thing is, you can still love someone and be wrong for them.",
+        "a": "Elvis Presley",
+        "c": "63",
+        "h": "\u003Cblockquote\u003E&ldquo;Sad thing is, you can still love someone and be wrong for them.&rdquo; &mdash; \u003Cfooter\u003EElvis Presley\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Rich people have small TVs and big libraries, and poor people have small libraries and big TVs.",
+        "a": "Zig Ziglar",
+        "c": "95",
+        "h": "\u003Cblockquote\u003E&ldquo;Rich people have small TVs and big libraries, and poor people have small libraries and big TVs.&rdquo; &mdash; \u003Cfooter\u003EZig Ziglar\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Success is stumbling from failure to failure with no loss of enthusiasm.",
+        "a": "Winston Churchill",
+        "c": "72",
+        "h": "\u003Cblockquote\u003E&ldquo;Success is stumbling from failure to failure with no loss of enthusiasm.&rdquo; &mdash; \u003Cfooter\u003EWinston Churchill\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "When you're curious, you find lots of interesting things to do.",
+        "a": "Walt Disney",
+        "c": "63",
+        "h": "\u003Cblockquote\u003E&ldquo;When you're curious, you find lots of interesting things to do.&rdquo; &mdash; \u003Cfooter\u003EWalt Disney\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Hope itself is like a star- not to be seen in the sunshine of prosperity, and only to be discovered in the night of adversity.",
+        "a": "Charles Spurgeon",
+        "c": "126",
+        "h": "\u003Cblockquote\u003E&ldquo;Hope itself is like a star- not to be seen in the sunshine of prosperity, and only to be discovered in the night of adversity.&rdquo; &mdash; \u003Cfooter\u003ECharles Spurgeon\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Not all those who wander are lost.",
+        "a": "J.R.R. Tolkien",
+        "c": "34",
+        "h": "\u003Cblockquote\u003E&ldquo;Not all those who wander are lost.&rdquo; &mdash; \u003Cfooter\u003EJ.R.R. Tolkien\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The finish line is just the beginning of a whole new race.",
+        "a": "Unknown",
+        "c": "58",
+        "h": "\u003Cblockquote\u003E&ldquo;The finish line is just the beginning of a whole new race.&rdquo; &mdash; \u003Cfooter\u003EUnknown\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Bloom where you are planted!",
+        "a": "Mary Engelbreit",
+        "c": "28",
+        "h": "\u003Cblockquote\u003E&ldquo;Bloom where you are planted!&rdquo; &mdash; \u003Cfooter\u003EMary Engelbreit\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Life was meant to be lived, and curiosity must be kept alive. One must never, for whatever reason, turn his back on life.",
+        "a": "Eleanor Roosevelt",
+        "c": "121",
+        "h": "\u003Cblockquote\u003E&ldquo;Life was meant to be lived, and curiosity must be kept alive. One must never, for whatever reason, turn his back on life.&rdquo; &mdash; \u003Cfooter\u003EEleanor Roosevelt\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "There are two ways of seeing: with the body and with the soul. The body's sight can sometimes forget, but the soul remembers forever.",
+        "a": "Alexandre Dumas",
+        "c": "133",
+        "h": "\u003Cblockquote\u003E&ldquo;There are two ways of seeing: with the body and with the soul. The body's sight can sometimes forget, but the soul remembers forever.&rdquo; &mdash; \u003Cfooter\u003EAlexandre Dumas\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Every man gotta right to decide his own destiny.",
+        "a": "Bob Marley",
+        "c": "48",
+        "h": "\u003Cblockquote\u003E&ldquo;Every man gotta right to decide his own destiny.&rdquo; &mdash; \u003Cfooter\u003EBob Marley\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The best way to not feel hopeless is to get up and do something.",
+        "a": "Barack Obama",
+        "c": "64",
+        "h": "\u003Cblockquote\u003E&ldquo;The best way to not feel hopeless is to get up and do something.&rdquo; &mdash; \u003Cfooter\u003EBarack Obama\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The mind is the limit.",
+        "a": "Arnold Schwarzenegger",
+        "c": "22",
+        "h": "\u003Cblockquote\u003E&ldquo;The mind is the limit.&rdquo; &mdash; \u003Cfooter\u003EArnold Schwarzenegger\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The quicker you let go of old cheese, the sooner you find new cheese.",
+        "a": "Spencer Johnson",
+        "c": "69",
+        "h": "\u003Cblockquote\u003E&ldquo;The quicker you let go of old cheese, the sooner you find new cheese.&rdquo; &mdash; \u003Cfooter\u003ESpencer Johnson\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Luck is a matter of preparation meeting opportunity.",
+        "a": "Oprah Winfrey",
+        "c": "52",
+        "h": "\u003Cblockquote\u003E&ldquo;Luck is a matter of preparation meeting opportunity.&rdquo; &mdash; \u003Cfooter\u003EOprah Winfrey\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The less people know, the more stubbornly they know it.",
+        "a": "Osho",
+        "c": "55",
+        "h": "\u003Cblockquote\u003E&ldquo;The less people know, the more stubbornly they know it.&rdquo; &mdash; \u003Cfooter\u003EOsho\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Trying to define yourself is like trying to bite your own teeth.",
+        "a": "Zen Proverb",
+        "c": "64",
+        "h": "\u003Cblockquote\u003E&ldquo;Trying to define yourself is like trying to bite your own teeth.&rdquo; &mdash; \u003Cfooter\u003EZen Proverb\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "You get in life what you have the courage to ask for.",
+        "a": "Oprah Winfrey",
+        "c": "53",
+        "h": "\u003Cblockquote\u003E&ldquo;You get in life what you have the courage to ask for.&rdquo; &mdash; \u003Cfooter\u003EOprah Winfrey\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "an action committed in anger is an action doomed to failure. ",
+        "a": "Genghis Khan",
+        "c": "61",
+        "h": "\u003Cblockquote\u003E&ldquo;an action committed in anger is an action doomed to failure. &rdquo; &mdash; \u003Cfooter\u003EGenghis Khan\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The meaning of life is to find your gift. The purpose of life is to give it away.",
+        "a": "Pablo Picasso",
+        "c": "81",
+        "h": "\u003Cblockquote\u003E&ldquo;The meaning of life is to find your gift. The purpose of life is to give it away.&rdquo; &mdash; \u003Cfooter\u003EPablo Picasso\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "We are all different. Don't judge, understand instead.",
+        "a": "Roy T. Bennett",
+        "c": "54",
+        "h": "\u003Cblockquote\u003E&ldquo;We are all different. Don't judge, understand instead.&rdquo; &mdash; \u003Cfooter\u003ERoy T. Bennett\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "First say to yourself what you would be; and then do what you have to do.",
+        "a": "Epictetus",
+        "c": "73",
+        "h": "\u003Cblockquote\u003E&ldquo;First say to yourself what you would be; and then do what you have to do.&rdquo; &mdash; \u003Cfooter\u003EEpictetus\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "In order to carry a positive action, we must develop here a positive vision.",
+        "a": "Dalai Lama",
+        "c": "76",
+        "h": "\u003Cblockquote\u003E&ldquo;In order to carry a positive action, we must develop here a positive vision.&rdquo; &mdash; \u003Cfooter\u003EDalai Lama\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "When you change what you believe, you change what you do.",
+        "a": "Spencer Johnson",
+        "c": "57",
+        "h": "\u003Cblockquote\u003E&ldquo;When you change what you believe, you change what you do.&rdquo; &mdash; \u003Cfooter\u003ESpencer Johnson\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "I put my heart and soul into my work, and I have lost my mind in the process.",
+        "a": "Vincent van Gogh",
+        "c": "77",
+        "h": "\u003Cblockquote\u003E&ldquo;I put my heart and soul into my work, and I have lost my mind in the process.&rdquo; &mdash; \u003Cfooter\u003EVincent van Gogh\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Nothing is permanent in this wicked world - not even our troubles.",
+        "a": "Charlie Chaplin",
+        "c": "66",
+        "h": "\u003Cblockquote\u003E&ldquo;Nothing is permanent in this wicked world - not even our troubles.&rdquo; &mdash; \u003Cfooter\u003ECharlie Chaplin\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "My definition of success is control.",
+        "a": "Kenneth Branagh",
+        "c": "36",
+        "h": "\u003Cblockquote\u003E&ldquo;My definition of success is control.&rdquo; &mdash; \u003Cfooter\u003EKenneth Branagh\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "To live is to be willing to die over and over again.",
+        "a": "Pema Chodron",
+        "c": "52",
+        "h": "\u003Cblockquote\u003E&ldquo;To live is to be willing to die over and over again.&rdquo; &mdash; \u003Cfooter\u003EPema Chodron\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.",
+        "a": "Dale Carnegie",
+        "c": "107",
+        "h": "\u003Cblockquote\u003E&ldquo;Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.&rdquo; &mdash; \u003Cfooter\u003EDale Carnegie\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Human happiness and moral duty are inseparably connected. ",
+        "a": "George Washington",
+        "c": "58",
+        "h": "\u003Cblockquote\u003E&ldquo;Human happiness and moral duty are inseparably connected. &rdquo; &mdash; \u003Cfooter\u003EGeorge Washington\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Use your smile to change the world but don't let the world change your smile.",
+        "a": "Unknown",
+        "c": "77",
+        "h": "\u003Cblockquote\u003E&ldquo;Use your smile to change the world but don't let the world change your smile.&rdquo; &mdash; \u003Cfooter\u003EUnknown\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Judge your success by what you had to give up in order to get it.",
+        "a": "Dalai Lama",
+        "c": "65",
+        "h": "\u003Cblockquote\u003E&ldquo;Judge your success by what you had to give up in order to get it.&rdquo; &mdash; \u003Cfooter\u003EDalai Lama\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The smallest of actions is always better than the noblest of intentions.",
+        "a": "Robin Sharma",
+        "c": "72",
+        "h": "\u003Cblockquote\u003E&ldquo;The smallest of actions is always better than the noblest of intentions.&rdquo; &mdash; \u003Cfooter\u003ERobin Sharma\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Action may not always bring happiness, but there is no happiness without action.",
+        "a": "William James",
+        "c": "80",
+        "h": "\u003Cblockquote\u003E&ldquo;Action may not always bring happiness, but there is no happiness without action.&rdquo; &mdash; \u003Cfooter\u003EWilliam James\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The pain of parting is nothing to the joy of meeting again.",
+        "a": "Charles Dickens",
+        "c": "59",
+        "h": "\u003Cblockquote\u003E&ldquo;The pain of parting is nothing to the joy of meeting again.&rdquo; &mdash; \u003Cfooter\u003ECharles Dickens\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Talk sense to a fool and he calls you foolish.",
+        "a": "Euripides",
+        "c": "46",
+        "h": "\u003Cblockquote\u003E&ldquo;Talk sense to a fool and he calls you foolish.&rdquo; &mdash; \u003Cfooter\u003EEuripides\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Life's too mysterious to take too serious.",
+        "a": "Mary Engelbreit",
+        "c": "42",
+        "h": "\u003Cblockquote\u003E&ldquo;Life's too mysterious to take too serious.&rdquo; &mdash; \u003Cfooter\u003EMary Engelbreit\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "If you take responsibility for yourself you will develop a hunger to accomplish your dream.",
+        "a": "Les Brown",
+        "c": "91",
+        "h": "\u003Cblockquote\u003E&ldquo;If you take responsibility for yourself you will develop a hunger to accomplish your dream.&rdquo; &mdash; \u003Cfooter\u003ELes Brown\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The truly rich men are the ones who have health, loved ones and a work they love.",
+        "a": "Maxime Lagace",
+        "c": "81",
+        "h": "\u003Cblockquote\u003E&ldquo;The truly rich men are the ones who have health, loved ones and a work they love.&rdquo; &mdash; \u003Cfooter\u003EMaxime Lagace\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "If you truly love Nature, you will find beauty everywhere. ",
+        "a": "Vincent van Gogh",
+        "c": "59",
+        "h": "\u003Cblockquote\u003E&ldquo;If you truly love Nature, you will find beauty everywhere. &rdquo; &mdash; \u003Cfooter\u003EVincent van Gogh\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "An ounce of performance is worth pounds of promises.",
+        "a": "Mae West",
+        "c": "52",
+        "h": "\u003Cblockquote\u003E&ldquo;An ounce of performance is worth pounds of promises.&rdquo; &mdash; \u003Cfooter\u003EMae West\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Avoiding danger is no safer in the long run than exposure.",
+        "a": "Helen Keller",
+        "c": "58",
+        "h": "\u003Cblockquote\u003E&ldquo;Avoiding danger is no safer in the long run than exposure.&rdquo; &mdash; \u003Cfooter\u003EHelen Keller\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "One must be deeply aware of the impermanence of the world.",
+        "a": "Dogen",
+        "c": "58",
+        "h": "\u003Cblockquote\u003E&ldquo;One must be deeply aware of the impermanence of the world.&rdquo; &mdash; \u003Cfooter\u003EDogen\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "All animals except man know that the principal business of life is to enjoy it.",
+        "a": "Samuel Butler",
+        "c": "79",
+        "h": "\u003Cblockquote\u003E&ldquo;All animals except man know that the principal business of life is to enjoy it.&rdquo; &mdash; \u003Cfooter\u003ESamuel Butler\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "The deeper the truth in a creative work, the longer it will live.  ",
+        "a": "Charlie Chaplin",
+        "c": "67",
+        "h": "\u003Cblockquote\u003E&ldquo;The deeper the truth in a creative work, the longer it will live.  &rdquo; &mdash; \u003Cfooter\u003ECharlie Chaplin\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Forget mistakes. Forget failure. Forget everything except what you are going to do now and do it. Today is your lucky day!",
+        "a": "Og Mandino",
+        "c": "122",
+        "h": "\u003Cblockquote\u003E&ldquo;Forget mistakes. Forget failure. Forget everything except what you are going to do now and do it. Today is your lucky day!&rdquo; &mdash; \u003Cfooter\u003EOg Mandino\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Words are clothes that thoughts wear",
+        "a": "Samuel Butler",
+        "c": "36",
+        "h": "\u003Cblockquote\u003E&ldquo;Words are clothes that thoughts wear&rdquo; &mdash; \u003Cfooter\u003ESamuel Butler\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Don't measure yourself by what you have accomplished, but by what you should have accomplished with your ability.",
+        "a": "John Wooden",
+        "c": "113",
+        "h": "\u003Cblockquote\u003E&ldquo;Don't measure yourself by what you have accomplished, but by what you should have accomplished with your ability.&rdquo; &mdash; \u003Cfooter\u003EJohn Wooden\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Hope is not a strategy. Luck is not a factor. Fear is not an option.",
+        "a": "James Cameron",
+        "c": "68",
+        "h": "\u003Cblockquote\u003E&ldquo;Hope is not a strategy. Luck is not a factor. Fear is not an option.&rdquo; &mdash; \u003Cfooter\u003EJames Cameron\u003C/footer\u003E\u003C/blockquote\u003E"
+      },
+      {
+        "q": "Instead of worrying about what you cannot control, shift your energy to what you can create.",
+        "a": "Roy T. Bennett",
+        "c": "92",
+        "h": "\u003Cblockquote\u003E&ldquo;Instead of worrying about what you cannot control, shift your energy to what you can create.&rdquo; &mdash; \u003Cfooter\u003ERoy T. Bennett\u003C/footer\u003E\u003C/blockquote\u003E"
+      }
   ]
